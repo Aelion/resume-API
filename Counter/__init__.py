d99e448e05f707b105a36a266e0f3475d6b7be0b
Mariusz <email@mariusza.pl>
2023-12-17 19:24:06 +0100

    .

--- Counter/__init__.py
+++ Counter/__init__.py
@@ -16,9 +16,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        #return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(visits())
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+def visits():
+    return 100
